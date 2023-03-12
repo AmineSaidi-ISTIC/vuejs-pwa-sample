99cbc9e7b5da782484d09e5b8a28242df2567278
AmineSaidi-ISTIC <59051511+AmineSaidi-ISTIC@users.noreply.github.com>
2023-03-12 19:30:41 +0100

    build

---
+++ dist/precache-manifest.da3387c9c09e59972edf58c56410b8e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950ad8b52a327de8f1ae",
+    "url": "/css/about.c770b961.css"
+  },
+  {
+    "revision": "b671176e5b83afb0ef99",
+    "url": "/css/chunk-vendors.7caec713.css"
+  },
+  {
+    "revision": "39cad07e1d0db4cbadbfb8fb012af2bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "950ad8b52a327de8f1ae",
+    "url": "/js/about.4d1d8619.js"
+  },
+  {
+    "revision": "0c3ae5f6c1a8a1d5cd37",
+    "url": "/js/app.423fdaec.js"
+  },
+  {
+    "revision": "b671176e5b83afb0ef99",
+    "url": "/js/chunk-vendors.53585b92.js"
+  },
+  {
+    "revision": "39a4a291b3fef4726232f1371fee0569",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
